477400c032c287ab6c20ca183550dcd55188c8a1
1122A72 <qq2346a9@yahoo.co.jp>
2025-02-24 22:12:46 +0900

    Add files via upload

--- kadai‐007/calc.js
+++ kadai‐007/calc.js
@@ -3,9 +3,9 @@ const num = 15;
 if (num % 3 === 0 && num % 5 === 0) {
   console.log("3と5の倍数です");  
 } else if (num % 3 === 0) {
-  console.log(num + "は、３の倍数です ");
+  console.log("３の倍数です");
 } else if (num % 5 === 0) {
-  console.log(num + "は、、5の倍数ですです");
+  console.log("5の倍数です");
 } else {
   console.log(num);
 }
